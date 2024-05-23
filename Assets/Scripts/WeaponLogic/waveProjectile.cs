a748b08c848ba411f828ca705b8befcf0622815c
TheGodseeker <pavrez.comp@gmail.com>
2024-05-23 17:14:50 +1000

    Залив

--- Assets/Scripts/WeaponLogic/waveProjectile.cs
+++ Assets/Scripts/WeaponLogic/waveProjectile.cs
@@ -7,9 +7,8 @@ public class WaveProjectile : MonoBehaviour
 {
     Vector3 direction; // направление движения
     [SerializeField] float speed; // скорость пули
-    //[SerializeField] float Shift = 20; // растояние смещения луча от игрока
     public int damage = 5; // урон пули
-     float Range = 6; // дальность пули
+    [SerializeField] float Range = 10.0f; // длина лучей в обе стороны
     
 
     
@@ -27,32 +26,12 @@ public class WaveProjectile : MonoBehaviour
         damage = settetDamage;
     }
     public void SetDirection() { //выбор цели для пули
-        //var playerPos = FindObjectsOfType<Player>();
         var playerPos = GameObject.FindWithTag("Player");
-
-        //direction = playerPos.transform.position;
-
-        /*
-        if (playerPos.GetComponent<Rigidbody2D>().velocity.x < 0)
-        {
-            direction = new Vector3(playerPos.transform.position.x + Shift, 
-                0,
-                playerPos.transform.position.z);
-        }
-        else
-        {
-            direction = new Vector3(playerPos.transform.position.x - Shift,
-                0,
-                playerPos.transform.position.z);
-        }
-        */
-
-        //{
             direction = new Vector3(playerPos.transform.position.x,
                 0,
                 playerPos.transform.position.z);
 
-            lifeTime = 60;
+        lifeTime = 30;
 
      }
         
@@ -62,6 +41,8 @@ public class WaveProjectile : MonoBehaviour
     private void Start() {
         bulletDirection = direction - this.transform.position;
         shotStart = this.transform.position;
+        this.transform.localScale = new Vector3(Range,
+            this.transform.localScale.y, this.transform.localScale.z);
     }
     // движение пули каждый кадр
     void Update() {
@@ -80,10 +61,12 @@ public class WaveProjectile : MonoBehaviour
     }
     void FixedUpdate() {
         if (Time.frameCount % 2 == 0) {
-            //Collider2D[] hit = Physics2D.OverlapCircleAll(transform.position, 0.1f);
-            Collider2D[] hit = Physics2D.OverlapCapsuleAll(transform.position,
+            var playerPos = GameObject.FindWithTag("Player").transform.position;
+
+            Collider2D[] hit = Physics2D.OverlapCapsuleAll(playerPos,//transform.position,
                 new Vector2(3.0f, 0.5f), CapsuleDirection2D.Horizontal,
                 0.0f);
+
             foreach (Collider2D c in hit) {
             Enemy enemy = c.GetComponent<Enemy>();
             if (enemy != null) {
@@ -92,13 +75,6 @@ public class WaveProjectile : MonoBehaviour
             }
         }
         }
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        //Output the Collider's GameObject's name
-        //UnityEngine.Debug.Log(collision.collider.name);
-        UnityEngine.Debug.Log(collision);
     }
 }
 
